1b48f0af2679541022c859326b5558a9999e1eb9
Atif_Khan <atif04011@gmail.com>
2024-08-20 18:35:42 +0530

    Arithmetic operations : Day02 of 30 days javascript challenge

---
+++ day02.js
@@ -0,0 +1,56 @@
+// Activity 1 Arithmetic operations
+
+// task 1 Write a program to add two numbers and log the result to the console
+
+
+const a = 20;
+const b = 10;
+const result = a+b;
+console.log(`the addition of a and b is ${result}`);
+
+// task 2 Write a program to subtract two numbers and log the result to the console
+
+const a2 = 20;
+const b2 = 10;
+const result2 = a2-b2;
+console.log(`the substraction of a2 and b2 is ${result2}`);
+
+// task 3 Write a program to multiply two numbers and log the result to the console
+
+const a3 = 3;
+const b3 = 2;
+const result3 = a3 * b3;
+console.log(`the multiplication of a3 and b3 is ${result3} `);
+
+
+
+// task 4 Write a program to divide two numbers and log the result to the console
+
+const a4 = 10;
+const b4 = 2;
+const result4 = a4 / b4;
+console.log(`the division of a4 and b4 is ${result4} `);
+
+
+
+// task 5 Write a program to find remainder when one number is divided by another and log the result to the console
+
+const a5 = 10;
+const b5 = 2;
+const result5 = a5 % b5;
+console.log(`the remainder by dividing a5 by b5 is ${result5} `);
+
+
+
+// Activity 2 Assignment operator 
+
+// task 6 use the += operator to add a number to a variable and log the result to the console
+let a6 = 6;
+a6 += 12;
+console.log(`the total value of a6 after addition using += operator is ${a6}`);
+
+
+// task 7 use the -= operator to subtract a number from a variable and log the result to the console
+let a7 = 12;
+a7 -= 6;
+console.log(`the total value of a7 after subtraction using -= operator is ${a7}`);
